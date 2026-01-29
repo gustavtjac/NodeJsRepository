47486e251cf54ea69dd0beaaf5bfffc32f6ced71
Gustav Jacobsen <gustavtorup@gmail.com>
2026-01-29 10:32:37 +0100

    update

--- 01._Misc/variables_1.js
+++ 01._Misc/variables_1.js
@@ -4,7 +4,20 @@
 
 
 //brug det rigtige variable assigner
+
+//const kan ikke ændre referencen men refrerencens værdi kan ændrer sig
 const luckyNumber = 5;
+
+
+
+const people =[];
+
+
+
+people.push("ostemad")
+people.push("Tisseost")
+
+console.log(people)
 
 //Count skal ændres
 let count = 2;
@@ -16,4 +29,8 @@ console.log(5)
 console.log(5)
 
 
+//JSDataTyper
+//String, Number, Boolean, BigInt, Object, null, undefined
 
+// AutomaticSemicolonInsertion
+//men behøver ikke semikolon
